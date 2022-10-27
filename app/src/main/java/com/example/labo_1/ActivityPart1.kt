7af2ba43126db80a9380a19a7dc9647533bf037c
Yohann Paulus <yohann.paulus@gmail.com>
2022-10-27 14:34:35 +0200

    Partie 1 presque finie

--- app/src/main/java/com/example/labo_1/ActivityPart1.kt
+++ app/src/main/java/com/example/labo_1/ActivityPart1.kt
@@ -1,21 +1,39 @@
 package com.example.labo_1
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+var storedName = ""
 class ActivityPart1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_part1)
-
         val edit = findViewById<Button>(R.id.editButton)
         edit.setOnClickListener {
             val intent = Intent(this, ActivityPart1B::class.java)
             startActivity(intent)
         }
+    }
+    override fun onResume() {
+        super.onResume()
+        if (intent.hasExtra("userName")) {
+            val name = intent.getStringExtra("userName")
+            val text = findViewById<TextView>(R.id.textView2);
+            val string = getString(R.string.hello)
+            text.text = "$string $name !"
+            storedName = name.toString()
+        }
+        else if (storedName != ""){
+            val text = findViewById<TextView>(R.id.textView2);
+            val string = getString(R.string.hello)
+            text.text = "$string $storedName !"
+        }
+    }
+    override fun onBackPressed() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
--- app/src/main/java/com/example/labo_1/ActivityPart1B.kt
+++ app/src/main/java/com/example/labo_1/ActivityPart1B.kt
@@ -5,17 +5,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ActivityPart1B : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_part1_b)
-
+        val text = findViewById<EditText>(R.id.editTextTextPersonName);
         val save = findViewById<Button>(R.id.save)
         save.setOnClickListener {
-            val intent = Intent(this, ActivityPart1::class.java)
-            startActivity(intent)
+            val name = text.text.toString()
+            if (name == "") {
+                Toast.makeText(applicationContext, "Merci de mettre ton prenom pour sauvegarder", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this, ActivityPart1::class.java)
+                intent.putExtra("userName", name);
+                startActivity(intent)
+            }
         }
     }
 }
