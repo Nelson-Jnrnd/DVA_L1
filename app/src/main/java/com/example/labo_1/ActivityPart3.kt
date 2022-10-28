38be9bc1ff0d3bd06e3c42882a050b753c22a61e
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-10-29 00:31:14 +0200

    activity 3

--- app/src/main/java/com/example/labo_1/ActivityPart3.kt
+++ app/src/main/java/com/example/labo_1/ActivityPart3.kt
@@ -1,18 +1,50 @@
 package com.example.labo_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActivityPart3 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_part3)
-        val fragmentContainerView = findViewById<androidx.fragment.app.FragmentContainerView>(R.id.fragmentContainerView)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .setReorderingAllowed(true)
-                .add(R.id.fragmentContainerView, StepFragment())
-                .commit()
+
+        val close = findViewById<Button>(R.id.close)
+        close.setOnClickListener {
+            finish()
         }
+
+        val next = findViewById<Button>(R.id.next)
+        next.setOnClickListener {
+            pushStep()
+        }
+
+        val previous = findViewById<Button>(R.id.previous)
+        previous.setOnClickListener {
+            popStep()
+        }
+
+        pushStep()
+    }
+
+    private fun popStep() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            finish()
+        }
+    }
+
+    private fun pushStep() {
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .add(
+                R.id.fragmentContainerView,
+                StepFragment.newInstance(supportFragmentManager.fragments.size + 1)
+            )
+            .addToBackStack(null)
+            .commit()
     }
 }
